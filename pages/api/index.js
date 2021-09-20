e68ed57f0166a087815f13b7a67d246d0e7139cc
unknown <komodoutd@gmail.com>
2021-09-20 19:29:07 +0200

    temporarily removed auth from homepage

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,9 @@
 import authenticate from '../../middleware/authenticate';
 
-export default authenticate(function handler(req, res) {
+export default function handler(req, res) {
   res.status(200).json({ message: 'Welcome to homepage' })
-})
+}
+
+// export default authenticate(function handler(req, res) {
+//   res.status(200).json({ message: 'Welcome to homepage' })
+// })
